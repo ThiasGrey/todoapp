046c68c3b8b2196934a071986d31687cb9d977a6
BM GREY <bm.grey@outlook.com>
2021-04-19 15:54:02 -0300

    all

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name="Sobre" small="Nós"></PageHeader>
+
+        <h2>Nossa História</h2>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia voluptatibus quaerat sunt odit fugiat autem quibusdam vero doloribus dolore aliquid aut, provident assumenda eveniet dolor numquam vitae esse minima delectus?</p>
+    
+    </div>
+)
---
+++ frontend/src/index.jsx
@@ -0,0 +1,8 @@
+import 'modules/bootstrap/dist/css/bootstrap.min.css'
+
+import './template/custon.css'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './main/app'
+
+ReactDOM.render(<App />, document.getElementById('app'))
